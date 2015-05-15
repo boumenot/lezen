fa8f2c759659c8a2983fb889b472045408f9d850
Christopher Boumenot <boumenot@gmail.com>
2015-05-14 21:58:35 -0700

    My first search engine.

---
+++ src/Lezen.Core/Search/SearchEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lezen.Core.Search
+{
+    public class SearchEngine
+    {
+        public IEnumerable<IDocument> Query(string q)
+        {
+            return Enumerable.Empty<IDocument>();
+        }
+    }
+}
---
+++ test/Lezen.Core.Test/Search/SearchEngineTest.cs
@@ -0,0 +1,21 @@
+﻿using Lezen.Core.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Lezen.Core.Test.Search
+{
+    public class SearchEngineTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var testSubject = new SearchEngine();
+            testSubject.Query(string.Empty).Should().BeEmpty();
+        }
+    }
+}
