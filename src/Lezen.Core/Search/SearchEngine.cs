dbe1c729e3391f22ab3ab646c1a8516e57681138
Christopher Boumenot <boumenot@gmail.com>
2015-05-14 22:45:00 -0700

    Implement query by EntityID.

--- src/Lezen.Core/Search/SearchEngine.cs
+++ src/Lezen.Core/Search/SearchEngine.cs
@@ -4,13 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+
 namespace Lezen.Core.Search
 {
-    public class SearchEngine
+    public class SearchEngine : IDisposable
     {
+        private readonly Directory directory;
+        private readonly StandardAnalyzer analyzer;
+        private readonly IndexWriter indexWriter;
+
+        private readonly DocumentFactory factory;
+
+        public SearchEngine(string path)
+        {
+            this.directory = FSDirectory.Open(path);
+            this.analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+            this.indexWriter = new IndexWriter(this.directory, analyzer, new IndexWriter.MaxFieldLength(int.MaxValue));
+
+            this.factory = new DocumentFactory();
+        }
+
         public IEnumerable<IDocument> Query(string q)
         {
             return Enumerable.Empty<IDocument>();
+        }
+
+        public IEnumerable<IDocument> Query(int entityID)
+        {
+            var term = new Term(Constants.Search.EntityID, entityID.ToString());
+            var q = new TermQuery(term);
+
+            return this.Query(q);
+        }
+
+        public void Insert(SearchItem searchItem) 
+        {
+            var doc = this.factory.Create(searchItem);
+            this.indexWriter.AddDocument(doc);
+        }
+
+        public void Dispose()
+        {
+            this.indexWriter.Commit();
+
+            using (this.indexWriter)
+            using (this.analyzer)
+            using (this.directory) { }
+        }
+
+        private IEnumerable<IDocument> Query(Query query)
+        {
+            using (var reader = this.indexWriter.GetReader())
+            using (var searcher = new IndexSearcher(reader))
+            {
+                var docs = searcher.Search(query, 100);
+
+                for (int i = 0; i < docs.TotalHits; i++)
+                {
+                    var offset = docs.ScoreDocs[i].Doc;
+                    var doc = reader[offset];
+
+                    yield return SearchDocument.Create(doc);
+                }
+            }
         }
     }
 }
--- test/Lezen.Core.Test/Search/SearchEngineTest.cs
+++ test/Lezen.Core.Test/Search/SearchEngineTest.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -12,10 +13,32 @@ namespace Lezen.Core.Test.Search
     public class SearchEngineTest
     {
         [Fact]
-        public void Test()
+        public void QueryShouldReturnDocumentsInSearchEngine()
         {
-            var testSubject = new SearchEngine();
-            testSubject.Query(string.Empty).Should().BeEmpty();
+            using (var temp = new TempDirectory(Directory.GetCurrentDirectory()))
+            using (var testSubject = new SearchEngine(temp.Path))
+            {
+                var searchItem1 = new SearchItem
+                {
+                    EntityID = 1,
+                    Abstract = "--abstract--",
+                    Keywords = new[] { "keyword1", "keyword2" },
+                    Text = "--text--",
+                };
+
+                var searchItem2 = new SearchItem { EntityID = 2, Abstract = "--abstract2--", Text = "--text2--", Keywords = new string[0] };
+
+                testSubject.Insert(searchItem1);
+                testSubject.Insert(searchItem2);
+
+                var documents = testSubject.Query(1);
+                documents.Should().HaveCount(1);
+
+                var document = documents.First();
+                document.EntityID.Should().Be(1);
+                document.Abstract.Should().Be("--abstract--");
+                document.Text.Should().Be("--text--");
+            }
         }
     }
 }
